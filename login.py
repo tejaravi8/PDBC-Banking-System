32940bf34141e6eb1f7f055cb895d547ca0e4998
tejaravi8 <botsaraviteja@gmail.com>
2025-09-21 23:54:51 +0530

    second time

--- login.py
+++ login.py
@@ -27,11 +27,27 @@ def login():
     
     option=int(input('\nChoose your option: '))
     
+    #1.withdraw money
+    
     if option==1:
         print(f'\n hello {uname}..\n')
         amount=int(input('Enter Withdraw Amount : '))
-        password=input('Enter Password : ')
+        password=input('\nEnter Password to confirm: ')
         if password==pswd:
             cursor.execute('update accounts set acc_bal=acc_bal-%s where user_id=%s',(amount,user_id,))
             mydb.commit()
-            print('successfully debited')
+            cursor.execute('select * from accounts where user_id=%s',(user_id,))
+            acdata=cursor.fetchone()
+            acc_id,user_id,acc_type,acc_bal=acdata
+            print(f'\nsuccessfully debited,your available balance is {acc_bal}')
+    
+    # 2.check balance
+    
+    if option==2:
+        print(f'\n hello {uname}..\n')
+        password=input('Enter Password to check : ')
+        if password==pswd:
+            cursor.execute('select acc_bal from accounts where user_id=%s',(user_id,))
+            acdata=cursor.fetchone()
+            acc_bal=acdata
+            print(f'\nyour current balance is {acc_bal}')
